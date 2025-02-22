6e0bc5ab6355be8570fc648dff287187893bd279
medidbella <mohamedidbellaali@gmail.com>
2025-02-22 17:30:53 +0100

    added ex04

---
+++ Week00/ex04/kata00.py
@@ -0,0 +1,5 @@
+kata = (19, 42, 21, 45, 6, 9)
+print(f"The {len(kata)} numbers are : ", end='')
+for i in range(len(kata)):
+	print(f"{kata[i]}{', ' if i < len(kata) - 1 else ''}", end='')
+print()
---
+++ Week00/ex04/kata02.py
@@ -0,0 +1,4 @@
+
+kata = (2019, 9, 25, 3, 30)
+formattedDate = (f"{kata[1]:02d}/{kata[2]:02d}/{kata[0]}")
+print(formattedDate + f" {kata[3]:02d}:{kata[4]:02d}")
