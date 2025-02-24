53f35c6d3b5ab7c184d8a67d04677c8f61903138
medidbella <mohamedidbellaali@gmail.com>
2025-02-24 12:39:10 +0000

    successfully scrapped the page to csv file

--- Week00/ex07/scrapper.py
+++ Week00/ex07/scrapper.py
@@ -4,9 +4,30 @@ import requests
 pageUrl = "https://data.1337ai.org/"
 response = requests.get(pageUrl)
 rawData = BeautifulSoup(response.text, 'html.parser')
-rows = rawData.find_all("th")
+tableHeaders = rawData.find_all("th")
+TableData = rawData.find_all("td")
+keys = [str(elm).replace("<th>", "").
+        replace("</th>", "") for elm in tableHeaders]
+Values = [str(elm).replace("<td>", "").
+          replace("</td>", "") for elm in TableData]
+csvFile = open("data.csv", 'w')
+index = 0
+keysNumber = len(keys)
 
-rowsText = []
-for elm in rows:
-    rowsText.append(str(elm).replace("<th>", "").replace("</th>", ""))
-print(rowsText)
+while index < keysNumber:
+    csvFile.write(keys[index])
+    if index != keysNumber - 1:
+        csvFile.write(", ")
+    index += 1
+index = 0
+csvFile.write('\n')
+
+
+while index < len(Values):
+    csvFile.write(Values[index])
+    if (index + 1) % keysNumber == 0:
+        csvFile.write('\n')
+    else:
+        csvFile.write(", ")
+    index += 1
+csvFile.write('\n')
