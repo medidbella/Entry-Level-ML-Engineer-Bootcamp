758f206b62f8431d42ec06892b9ae52c0fe7ff5d
medidbella <mohamedidbellaali@gmail.com>
2025-02-24 06:53:08 +0000

    proper progress bar format

--- Week00/ex06/loading.py
+++ Week00/ex06/loading.py
@@ -1,8 +1,7 @@
 from time import sleep, time
-from sys import stdout
+from sys import stdout, exit
 from tqdm import tqdm
 
-#ETA: 8.67s [ 23%][=====>                    ] 233/1000 | elapsed time 2.33s
 
 progressBarLen = 26
 
@@ -12,8 +11,8 @@ def progress_visualizer(iteration, elapsedTime, iterationsNumber):
 		return
 	progressChars = progress / 100 * progressBarLen
 	ETA = (elapsedTime / progress * 100) - elapsedTime
-	bar = ('=' * int(progressChars - 1) + '>').ljust((progressBarLen), ' ')
-	print(f"ETA: {ETA:.2f}s [ {int(progress)}%][{bar}]",end='')
+	bar = ('=' * int(progressChars - 1) + ('>' if progress < 100 else '=')).ljust((progressBarLen), ' ')
+	print(f"ETA: {ETA:.2f}s [{int(progress):3}%][{bar}]",end='')
 	print(f"{iteration}/{iterationsNumber} | elapsed time {elapsedTime:.2f}s", end='\r')
 
 def ft_progress(lst):
@@ -21,14 +20,13 @@ def ft_progress(lst):
 	t0 = time()
 	elapsedTime = 0
 	iterationsNumber = len(lst)
+
 	for elm in lst:
 		elapsedTime = time() - t0
 		callTime = time()
 		yield elm
 		progress_visualizer(iteration, elapsedTime, iterationsNumber)
 		iteration += 1
-	print()
-
 
 tab = range(1000)
 
@@ -36,4 +34,5 @@ number = 0
 for elm in ft_progress(tab):
 	number += elm
 	sleep(0.01)
+print()
 print(number)
