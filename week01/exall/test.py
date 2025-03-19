6a3d5a068a9fd799a1733cf75485c76102d72bce
medidbella <mohamedidbellaali@gmail.com>
2025-03-19 21:10:33 +0000

    added scatter plots

--- week01/exall/test.py
+++ week01/exall/test.py
@@ -1,9 +1,18 @@
 import numpy
 import pandas
 import matplotlib.pyplot as plt
+import seaborn as sbrn
 
 db = pandas.read_csv("../Housing.csv")
 db = db.dropna()
 db = db.drop_duplicates()
-plt.boxplot(db["price"])
+
+# for field in db.columns:
+#     if field == "price":
+#         continue
+#     plt.title("price in relation to " + field)
+#     plt.scatter(db["price"], db[field])
+#     plt.show()
+
+sbrn.heatmap([db["price"] [db["stories"]]])
 plt.show()
